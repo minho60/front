f6c7318b2d027b764fbc059c21ce5e948b61db11
minho60 <minho031@gmail.com>
2025-12-12 09:13:40 +0900

    1211

--- megabox/js/common.js
+++ megabox/js/common.js
@@ -2,10 +2,18 @@
 //스토어 메뉴는 하위 메뉴가 없으므로 제외
 $('.main li:not(:nth-of-type(5)').mouseenter((e)=>{
     $('#header').addClass('active');
+    $(this).find('.sub').addClass('active');
 });
-$('.main li').mouseleave(()=>{
+
+$('#header > li').mouseleave(()=>{
+    $('#gnb .sub').removeClass('active');
+});
+
+$('#header .subbar').mouseleave(()=>{
     $('#header').removeClass('active');
 });
+
+
 
 // footer - looking for theater
 $(".btn_looking_theater").on("click", () => {
