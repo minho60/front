05d5956ac6226c001eae79d5882d2ed8365be6bf
minho60 <minho031@gmail.com>
2025-12-12 16:01:30 +0900

    1212

--- megabox/js/common.js
+++ megabox/js/common.js
@@ -1,19 +1,22 @@
 // header 2depth background
-//스토어 메뉴는 하위 메뉴가 없으므로 제외
-$('.main li:not(:nth-of-type(5)').mouseenter((e)=>{
-    $('#header').addClass('active');
-    $(this).find('.sub').addClass('active');
+// :not(:nth-of-type(5)
+// -> 스토어 메뉴는 하위 메뉴가 없으므로 제외
+$(".main > li:not(:nth-of-type(5)").mouseenter(e => {
+    $(e.currentTarget).find(".sub").addClass("active");
+    $("#header").addClass("active");
 });
 
-$('#header > li').mouseleave(()=>{
-    $('#gnb .sub').removeClass('active');
+$(".main > li").mouseleave(() => {
+    $("#header").removeClass("active");
+    $('.sub').removeClass("active");
 });
 
-$('#header .subbar').mouseleave(()=>{
-    $('#header').removeClass('active');
+$("#header .subbar").mouseenter(() => {
+    $("#header").addClass("active");
 });
-
-
+$("#header .subbar").mouseleave(() => {
+    $("#header").removeClass("active");
+});
 
 // footer - looking for theater
 $(".btn_looking_theater").on("click", () => {
